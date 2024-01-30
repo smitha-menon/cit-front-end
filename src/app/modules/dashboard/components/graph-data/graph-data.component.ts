3311a6801001c5583c917fb9362c8c30bdc68171
smitha menon <67955814+smitha-menon@users.noreply.github.com>
2024-01-30 12:07:22 +0530

    count fielname change,dashboard changes

--- src/app/modules/dashboard/components/graph-data/graph-data.component.ts
+++ src/app/modules/dashboard/components/graph-data/graph-data.component.ts
@@ -33,7 +33,7 @@ export class GraphDataComponent implements OnInit{
   ctx2:any;
   ctx3:any
   bargraphData!: any;
-  verticalBargraphData: any;
+  verticalBargraphData: any = [50, 20, 30];
   optionSelected: any;
   public doughnutChartLabels1: Label[] = ['Reused', 'modifed', 'new'];
   public doughnutChartData1: MultiDataSet = [[50, 20, 30]];
@@ -43,6 +43,7 @@ export class GraphDataComponent implements OnInit{
       enabled: true
     }
   };
+  public verticalBargraphLabel: Label[]=['january','february','march','april'];
   /**
    *
    */
@@ -54,7 +55,21 @@ export class GraphDataComponent implements OnInit{
       {
         console.log('option',params['option']);
         this.optionSelected=params['option'];
+        if( this.optionSelected==1)
+        {
+        this.loadData();
+        }
+       
+        if( this.optionSelected==3)
+        {
+          this.loadDataOption3();        
         
+        }
+      
+         if( this.optionSelected==2)
+        { 
+            this.loadOption2();
+        }    
       }
      
 });
@@ -64,72 +79,23 @@ export class GraphDataComponent implements OnInit{
     this.permissionsService.loginreponse$.subscribe((data) => {
       this.selectedGroup = data.currentGroupData.assignedGroupId;
     });
-
-    this.loadData();
+   
+    console.log("inside init for option", this.optionSelected);
   }
   ngAfterViewInit(): void {
+   
     if( this.optionSelected==4)
     {
-    this.renderStackedBarChart();
-    }
+      this.renderStackedBarChart();
+    } 
    
     // this.canvas1= this.mychart.nativeElement
     // this.ctx1=this.canvas1.getContext('2d');
     // this.canvas2= this.mychart1.nativeElement
     // this.ctx2=this.canvas2.getContext('2d');
     console.log("ctx1",this.ctx1);
-    if( this.optionSelected==3)
-    {
-      this.ctx2=document.getElementById('mychart1') as HTMLCanvasElement;
-    new Chart(this.ctx2,{
-      type: 'horizontalBar',
-     data:{
-        datasets:[{
-          label: 'Priority1',
-          data: [0,20,40,50],
-          backgroundColor:"rgb(115,185,243 / 65%)",
-          borderColor:"#007ee7",
-          fill: true,},
-          {
-          label: 'Priority2',
-          data: [0,20,40,60,80],
-          backgroundColor:"#47a0e8",
-          borderColor:"#007ee7",
-          fill: true,
-
-        }],
-        labels:['Application1','Application2','Application3','Application4']
-      }
-    });
-  }
-  if( this.optionSelected==2)
-  {
-    this.ctx1 = document.getElementById('mychart') as HTMLCanvasElement;
-    console.log("ctx1",this.ctx1);
-    new Chart(this.ctx1,{
-      type: 'bar',
-      data:{
-        labels: ['january','february','march','april'],
-        datasets: [
-          {
-            label: 'Incidents by Team',
-            data: [0,20,40,50],
-            fill: false,
-            borderColor: '#007ee7',
-            backgroundColor: "rgb(115,185,243 / 65%)",
-            borderWidth: 1,
-            pointStyle: 'circle',
-            pointRadius: 5,
-            pointHoverRadius: 15
-            // pointStyle: 'rectRot',
-            // pointRadius: 5,
-            // pointBorderColor: 'rgb(0, 0, 0)'
-          },
-        ]
-      }
-      
-    });
-  }
+   
+  
 
     
   }
@@ -271,57 +237,147 @@ export class GraphDataComponent implements OnInit{
   
   }
 
-    loadBarChart()
-    {
-      //const cconfig:ChartConfiguration= {"indexAxis": 'y' }
-      new Chart("myChart3", {
-        type: 'line',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
-        },
+    // loadBarChart()
+    // {
+    //   //const cconfig:ChartConfiguration= {"indexAxis": 'y' }
+    //   new Chart("myChart3", {
+    //     type: 'line',
+    //     data: {
+    //       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //       datasets: [{
+    //         label: '# of Votes',
+    //         data: [12, 19, 3, 5, 2, 3],
+    //         borderWidth: 1
+    //       }]
+    //     },
        
-        // options: {
-        //   scales: {
-        //     // y: {
-        //     //   beginAtZero: true
-        //     // }
-        //   }
-        // }
-      });
+    //     // options: {
+    //     //   scales: {
+    //     //     // y: {
+    //     //     //   beginAtZero: true
+    //     //     // }
+    //     //   }
+    //     // }
+    //   });
+    // }
+
+    loadOption2(){
+      this.apiservices.getIncidentByUserData(this.selectedGroup).subscribe({
+        next:(response:any)=>{
+          console.log("dashboard",response);
+          this.verticalBargraphData=response?.map((data:any) => data.incidentsCount); 
+          this.verticalBargraphLabel =response?.map((data:any) => data.username); 
+          console.log("this.verticalBargraphLabel",this.verticalBargraphLabel);
+          this.loadchartOption2();
+        },
+        error:(err:any)=>{console.log(err)}
+      });   
+
     }
 
-    loadData()
-    {
+    loadchartOption2(){
+      this.ctx1 = document.getElementById('mychart') as HTMLCanvasElement;
+      console.log("verticalBargraphLabel",this.verticalBargraphLabel);
+      new Chart(this.ctx1,{
+        type: 'bar',
+        data:{
+          labels: this.verticalBargraphLabel,//['january','february','march','april'],
+          datasets: [
+            {
+              label: 'Incidents by Team',
+              data: this.verticalBargraphData, //[0,20,40,50],
+              fill: false,
+              borderColor: '#007ee7',
+              backgroundColor: "rgb(115,185,243 / 65%)",
+              borderWidth: 1,
+              
+            },
+          ]
+        }
+        
+      });
 
+    }
+
+    loadDataOption3(){
       this.apiservices.getIncidentByPriority(this.selectedGroup).subscribe({
         next:(response:any)=>{
           console.log("dashboard",response);
           this.dashBoardData=response;
+          this.loadchartOption3();
         },
         error:(err:any)=>{console.log(err)}
       });
+    }
+
+    loadchartOption3(){
+
+      const labels=this.dashBoardData?.map((data:any) => data.applicationName);
+      let itemlabels = this.dashBoardData?.map((item:any) => item.data.map((x:any) => x.priority));
+      
+      let configData: any=[];
+      let itemcount:any =[];
+      // console.log("itemlabels",itemlabels);
+     
+
+       itemlabels[0]?.forEach((element:any) => {    
+       
+        itemcount=[];        
+        const v= this.dashBoardData.forEach((y:any) => {
+          itemcount.push(y.data.find((x:any) => (x.priority===element))?.count)
+        });
+        console.log("find",itemcount);
+            configData.push({
+                    label: element,
+                    data: itemcount,
+                    backgroundColor:"rgb(115,185,243 / 65%)",
+                    borderColor:"#007ee7",
+                    fill: true,});
+
+                // });
+        
+        });
+     console.log("configData",configData);
+      this.ctx2=document.getElementById('mychart1') as HTMLCanvasElement;
+    new Chart(this.ctx2,{
+      type: 'horizontalBar',
+     data:{
+      datasets:configData,
+        // datasets:[{
+        //   label: 'Priority1',
+        //   data: [0,20,40,50],
+        //   backgroundColor:"rgb(115,185,243 / 65%)",
+        //   borderColor:"#007ee7",
+        //   fill: true,},
+        //   {
+        //   label: 'Priority2',
+        //   data: [0,20,40,60,80],
+        //   backgroundColor:"#47a0e8",
+        //   borderColor:"#007ee7",
+        //   fill: true,
+
+        // }],
+        labels:labels//['Application1','Application2','Application3','Application4']
+      }
+    });
+    }
+
+    loadData()
+    {
+      
       this.apiservices.getIncidentByApplnData(this.selectedGroup).subscribe({
         next:(response:any)=>{
           console.log("dashboard",response);
           this.bargraphData=response;
           this.doughnutChartLabels1=response?.map((data:any) => data.applicationName); 
           this.doughnutChartData1=response?.map((data:any) => data.incidentsNumber); 
-          console.log("dashboard1",this.doughnutChartLabels1);
-          console.log("dashboard2",this.doughnutChartData1);
+          // console.log("dashboard1",this.doughnutChartLabels1);
+          // console.log("dashboard2",this.doughnutChartData1);
         },
         error:(err:any)=>{console.log(err)}
       });
-      this.apiservices.getIncidentByUserData(this.selectedGroup).subscribe({
-        next:(response:any)=>{
-          console.log("dashboard",response);
-          this.verticalBargraphData=response;
-        },
-        error:(err:any)=>{console.log(err)}
-      });
+     
     }
+
+    
 }
